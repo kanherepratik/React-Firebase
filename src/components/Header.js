3a72eae20fd3a2f72351a943e3a08a6ff12d54d6
Prateek Kanhere <kanherepratik@gmail.com>
2017-07-13 02:36:08 +0530

    Add files via upload

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from '../logo.svg';
 
-export default class Form extends Component {
+export default class Header extends Component {
     render() {
         return (
                 <div className = "App-header"> <img src = {logo} className = "App-logo" alt = "logo" /> 
--- src/index.js
+++ src/index.js
@@ -4,18 +4,25 @@ import './index.css';
 import App from './App';
 import Login from './components/Login'
 import SignUp from './components/SignUp'
-import Header from './components/Header'
-import registerServiceWorker from './registerServiceWorker';
-import {Router, Route} from 'react-router';
-import { BrowserRouter } from 'react-router-dom'
+import Dashboard from './components/Dashboard'
+import {Router, Route, browserHistory, IndexRoute} from "react-router";
 
-ReactDOM.render(
-    <BrowserRouter>
-        <div>
-            <Route path='/' component={App}></Route>
-            <Route path='/Home' component={Login}></Route>
-        </div>
-    </BrowserRouter>,
-    document.getElementById('root')
-)
-registerServiceWorker();
+    class Root extends React.Component {
+        render() {
+        return (
+            <Router history={browserHistory}>
+                <Route path={"/"} component={App} >
+                    <IndexRoute component={Login} />
+                    <Route path={"register"} component={SignUp} />
+                    <Route path={"dashboard"} component={Dashboard} />
+                </Route>
+            </Router>
+        );
+    }
+    }
+    
+    ReactDOM.render(<Root />, document.getElementById('root'));
+   
+    
+
+
